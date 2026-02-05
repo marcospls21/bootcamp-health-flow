39d456e5725228b3fb9dfe3e8b99b02b65052b84
marcospls21 <marcossouza_19@hotmail.com>
2026-02-05 19:35:43 -0300

    adicionando front

--- src/core-app/app.py
+++ src/core-app/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+# Define que a pasta de templates está no diretório local
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def hello():
-    return "Health Flow Core System - Prontuário Eletrônico v1.0"
+def home():
+    # Renderiza a página inicial (index.html)
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index_redirect():
+    return render_template('index.html')
+
+@app.route('/login.html')
+def login():
+    # Renderiza a página de login
+    return render_template('login.html')
+
+# Rota de verificação de saúde (Health Check) para o Kubernetes
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
